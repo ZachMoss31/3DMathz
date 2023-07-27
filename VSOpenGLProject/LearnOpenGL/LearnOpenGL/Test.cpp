910a347a5ce41b504989b68d281cc1e9ad9b4ecf
Zach Moss <ganondorf4prez@gmail.com>
2023-07-27 11:57:20 -0600

    Second Shader w/ Files
    Was able to add the secondShader object with it's second triangle to draw. It loads in the green color, but the next challenge is learning how to get the glow effect without using a global uniform4 variable.

--- VSOpenGLProject/LearnOpenGL/LearnOpenGL/Test.cpp
+++ VSOpenGLProject/LearnOpenGL/LearnOpenGL/Test.cpp
@@ -33,27 +33,21 @@ int main() {
 		return -1;
 	}
 	
-	//-------------------------------------------------------- SHADER -------
+	//-------------------------------------------------------- SHADERS -------
 	Shader ourShader("3.3.shader.vs", "3.3.shader.fs");
+	Shader secondShader("3.3.shader.vs", "GreenGlow.fs");
 
-	//Our Triangle
+	//First Triangle
 	float vertices[] = {
 		-0.9f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
 		0.0f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
 		-0.45f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
 	};
 
-	float vertices2[] = {
-		0.0f, 0.5f, 0.0f,
-		0.9f, 0.5f, 0.0f,
-		0.45f, -0.5f, 0.0f
-	};
-
 	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glBindVertexArray(VAO);
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	
@@ -64,13 +58,25 @@ int main() {
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-		////Older glowing shader
-		
-	//glBindVertexArray(VAOs[1]);
-	//glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
-	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-	//glEnableVertexAttribArray(0);
+
+
+	//Glowing Shader
+	 
+	//Second Triangle
+	float flipTri[] = {
+		0.0f, 0.5f, 0.0f,
+		0.9f, 0.5f, 0.0f,
+		0.45f, -0.5f, 0.0f
+	};
+
+	unsigned int VBO2, VAO2;
+	glGenVertexArrays(1, &VAO2);
+	glGenBuffers(1, &VBO2);
+	glBindVertexArray(VAO2);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(flipTri), flipTri, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(0);
 
 	/*float timeValue = glfwGetTime();
 	float greenValue = sin(timeValue) / 2.0f + 0.5f;
@@ -85,31 +91,27 @@ int main() {
 		glClearColor(0.5f, 0.0f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		
+		//First Triangle
 		ourShader.Use();
-		//timeValue = glfwGetTime();
-		//greenValue = sin(timeValue) / 2.0f + 0.5f;
-		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		
-		//Draw the Blue Triangle as Wireframe
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		//glUseProgram(shaderProgram2);
-		//glBindVertexArray(VAOs[1]);
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		//Second Triangle
+		//timeValue = glfwGetTime();
+		//greenValue = sin(timeValue) / 2.0f + 0.5f;
+		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		secondShader.Use();
+		glBindVertexArray(VAO2);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-	
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-
-	//glDeleteProgram(shaderProgram);
-	//glDeleteProgram(shaderProgram2);
+	glDeleteVertexArrays(1, &VAO2);
+	glDeleteBuffers(1, &VBO2);
 
 	glfwTerminate();
 	return 0;
